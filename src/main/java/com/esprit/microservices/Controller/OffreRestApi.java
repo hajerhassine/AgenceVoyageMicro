920cd7c67801e64b734cac0b3c58021851ccff15
lassouedhamza <43931046+lassouedhamza@users.noreply.github.com>
2022-10-21 22:50:45 +0100

    crud image

--- src/main/java/com/esprit/microservices/Controller/OffreRestApi.java
+++ src/main/java/com/esprit/microservices/Controller/OffreRestApi.java
@@ -2,7 +2,10 @@ package com.esprit.microservices.Controller;
 
 import java.util.List;
 
+import javax.ws.rs.core.Response;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonParseException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,11 +13,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.esprit.microservices.Entity.Offre;
 import com.esprit.microservices.Service.OffreService;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.MediaType;
@@ -28,10 +37,15 @@ public class OffreRestApi {
 
 	// URL: http://localhost:8086/SpringMVC/servlet/offre/addoffre
 
-	@PostMapping("/addoffre")
-	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<Offre> createoffre(@RequestBody Offre mh) {
-		return new ResponseEntity<>(offreservice.addoffre(mh), HttpStatus.OK);
+//	@PostMapping("/addoffre")
+//	@ResponseStatus(HttpStatus.CREATED)
+//	public ResponseEntity<Offre> createoffre(@RequestBody Offre mh) {
+//		return new ResponseEntity<>(offreservice.addoffre(mh), HttpStatus.OK);
+//	}
+	@PostMapping("/add")
+	@ResponseBody
+	public ResponseEntity<Response>  addHotel(@RequestPart("file") MultipartFile file,@RequestParam("offre") String s)throws JsonParseException, JsonMappingException, Exception {
+		 return offreservice.add(file,s);
 	}
 
 	@GetMapping("/getalloffre")
--- src/main/java/com/esprit/microservices/Service/OffreService.java
+++ src/main/java/com/esprit/microservices/Service/OffreService.java
@@ -1,23 +1,87 @@
 package com.esprit.microservices.Service;
 
+import java.io.File;
 import java.util.List;
 
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Response;
+
+import java.util.Optional;
+
+
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.esprit.microservices.Entity.Offre;
 import com.esprit.microservices.Repository.OffreRepository;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class OffreService {
 
 	@Autowired
 	public OffreRepository offrerepo;
+	
+	@Autowired
+    ServletContext context;
 
-	public Offre addoffre(Offre mh) {
+//	public Offre addoffre(Offre mh) {
+//
+//		return offrerepo.save(mh);
+//	}
+	public ResponseEntity<Response> add(MultipartFile file, String Offre) throws JsonParseException, JsonMappingException, Exception
+    {
 
-		return offrerepo.save(mh);
-	}
+        System.out.println("Ok .............");
+        Offre s = new ObjectMapper().readValue(Offre, Offre.class);
+        boolean isExit = new File(context.getRealPath("/Images/")).exists();
+        if (!isExit)
+        {
+            new File (context.getRealPath("/Images/")).mkdir();
+            System.out.println("mkdir success.............");
+        }
+        String filename = file.getOriginalFilename();
+        String newFileName = FilenameUtils.getBaseName(filename)+"."+FilenameUtils.getExtension(filename);
+        File serverFile = new File (context.getRealPath("/Images/"+File.separator+newFileName));
+        try
+        {
+            System.out.println("Image");
+            FileUtils.writeByteArrayToFile(serverFile,file.getBytes());
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
+
+        s.setImage(newFileName);
+
+        Offre art = offrerepo.save(s);
+
+
+
+        if (art != null)
+        {
+            return new ResponseEntity<Response>( HttpStatus.OK);
+        }
+        else
+        {
+            return new ResponseEntity<Response>(HttpStatus.BAD_REQUEST);
+        }
+
+
+
+    }
 
 	public List<Offre> Alloffre() {
 		List<Offre> offres = (List<Offre>) offrerepo.findAll();
